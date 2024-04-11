70b32e31bc868bef9ef72408dd6a1ee549f1d80b
martinsharma17 <maritnsharma18@gmail.com>
2024-04-11 15:21:13 +0545

    color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -52,7 +52,7 @@ export function SidebarItem({ icon, text, active, alert }) {
     return (
 
 
-        <li className={` relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "hover:bg-indigo-50 text-gray-600"}`}>
+        <li className={` relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${active ? "bg-gradient-to-tr bg-green-400 to-indigo-100 text-black-800" : "hover:bg-green-400  text-black-600"}`}>
             {icon}
             <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>{text}</span>
             {alert && (
